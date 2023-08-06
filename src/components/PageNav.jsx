48d0e188a7e4c94263eb34eba2d1ababe6fd1381
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-08-06 03:34:26 +0100

    update

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,23 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
 function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Homepage</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,16 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import styles from "./Homepage.module.css";
 
-function Homepage() {
+export default function Homepage() {
   return (
-    <div>
+    <main className={styles.homepage}>
       <PageNav />
 
-      <h1>WorldWise</h1>
-
-      <NavLink to="/pricing">Pricing</NavLink>
-    </div>
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link to="/app" className="cta">
+          Start tracking now
+        </Link>
+      </section>
+    </main>
   );
 }
-
-export default Homepage;
