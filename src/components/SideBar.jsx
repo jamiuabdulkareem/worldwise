c05f572b2f54427708ed04822288f50cbce56d56
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-08-06 03:59:02 +0100

    update

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,21 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import styles from "./Sidebar.module.css";
+
 function SideBar() {
-  return <div></div>;
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <footer className={styles.footer}>
+        <p className={styles.copyright}>
+          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default SideBar;
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,10 +1,13 @@
 import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import SideBar from "../components/SideBar";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
-      <p>App</p>
+    <div className={styles.app}>
+      <SideBar />
+      <Map />
     </div>
   );
 }
