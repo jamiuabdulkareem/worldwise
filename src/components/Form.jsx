65111345106bf2954296b5884c04b62c37b48852
Jamiu Abidemi Abdulkareem <106557387+jamiuabdulkareem@users.noreply.github.com>
2023-08-13 02:04:29 +0100

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -66,12 +66,17 @@ function Form() {
     [lat, lng]
   );
 
+  function handleSubmit() {}
+
   if (isLoadingGeocoding) return <Spinner />;
+
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map" />;
 
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
